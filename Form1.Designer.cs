9e074c1d20d06cfd179de7b5a78a8472f2a94d90
Mehmet Kıran <57233999+mehmetkrn@users.noreply.github.com>
2025-06-10 22:11:44 +0300

    Async_Await_Example First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Async_Await_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnKahve = new System.Windows.Forms.Button();
+            this.BtnCay = new System.Windows.Forms.Button();
+            this.BtnFiltreKahve = new System.Windows.Forms.Button();
+            this.Lst = new System.Windows.Forms.ListBox();
+            this.BtnHazirla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnKahve
+            // 
+            this.BtnKahve.Location = new System.Drawing.Point(350, 14);
+            this.BtnKahve.Name = "BtnKahve";
+            this.BtnKahve.Size = new System.Drawing.Size(146, 52);
+            this.BtnKahve.TabIndex = 0;
+            this.BtnKahve.Text = "Kahve";
+            this.BtnKahve.UseVisualStyleBackColor = true;
+            // 
+            // BtnCay
+            // 
+            this.BtnCay.Location = new System.Drawing.Point(502, 12);
+            this.BtnCay.Name = "BtnCay";
+            this.BtnCay.Size = new System.Drawing.Size(146, 52);
+            this.BtnCay.TabIndex = 1;
+            this.BtnCay.Text = "Çay";
+            this.BtnCay.UseVisualStyleBackColor = true;
+            // 
+            // BtnFiltreKahve
+            // 
+            this.BtnFiltreKahve.Location = new System.Drawing.Point(654, 12);
+            this.BtnFiltreKahve.Name = "BtnFiltreKahve";
+            this.BtnFiltreKahve.Size = new System.Drawing.Size(146, 52);
+            this.BtnFiltreKahve.TabIndex = 2;
+            this.BtnFiltreKahve.Text = "Filtre Kayve";
+            this.BtnFiltreKahve.UseVisualStyleBackColor = true;
+            // 
+            // Lst
+            // 
+            this.Lst.FormattingEnabled = true;
+            this.Lst.Location = new System.Drawing.Point(350, 72);
+            this.Lst.Name = "Lst";
+            this.Lst.Size = new System.Drawing.Size(450, 368);
+            this.Lst.TabIndex = 3;
+            // 
+            // BtnHazirla
+            // 
+            this.BtnHazirla.Location = new System.Drawing.Point(12, 15);
+            this.BtnHazirla.Name = "BtnHazirla";
+            this.BtnHazirla.Size = new System.Drawing.Size(146, 51);
+            this.BtnHazirla.TabIndex = 4;
+            this.BtnHazirla.Text = "Hazırla";
+            this.BtnHazirla.UseVisualStyleBackColor = true;
+            this.BtnHazirla.Click += new System.EventHandler(this.BtnHazirla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 450);
+            this.Controls.Add(this.BtnHazirla);
+            this.Controls.Add(this.Lst);
+            this.Controls.Add(this.BtnFiltreKahve);
+            this.Controls.Add(this.BtnCay);
+            this.Controls.Add(this.BtnKahve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnKahve;
+        private System.Windows.Forms.Button BtnCay;
+        private System.Windows.Forms.Button BtnFiltreKahve;
+        private System.Windows.Forms.ListBox Lst;
+        private System.Windows.Forms.Button BtnHazirla;
+    }
+}
+
